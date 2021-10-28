a29a3218bae7095956590ccca2c57b784db3e922
komatinenivs <89587840+komatinenivs@users.noreply.github.com>
2021-10-27 22:00:23 -0500

    started working on heroku

---
+++ routes/mydata.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Venkata Sravani Komatineni' });
+});
+
+module.exports = router;
